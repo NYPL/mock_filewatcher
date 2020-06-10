6907f7de5284e1121105723eaf3a5a00770ea1ad
Bobby Wilson <bobbywilson0@gmail.com>
2020-06-10 14:48:20 -0600

    update syntax for latest Faker

--- lib/mock_filewatcher.rb
+++ lib/mock_filewatcher.rb
@@ -15,9 +15,9 @@ class MockFilewatcher
     throw "unsupported :message_version" unless SUPPORTED_VERSIONS.include?(options[:message_version])
     throw "unsupported :bag_disposition" unless SUPPORTED_DISPOSITIONS.include?(options[:bag_disposition])
 
-    @bag_path  = "/bags/#{Faker::Lorem.words(2).join("/")}"
+    @bag_path  = "/bags/#{Faker::Lorem.words(number: 2).join("/")}"
 
-    file_base = Faker::Lorem.words(2).join
+    file_base = Faker::Lorem.words(number: 2).join
     extension = options[:bag_disposition] == "video" ? 'mov' : 'wav'
     @file_name = "#{file_base}.#{extension}"
     @options = options
@@ -37,11 +37,11 @@ private
       @message[:pathToAsset]         = "#{@bag_path}/#{@file_name}"
       @message[:metadata][:fileName] = File.basename(@file_name, ".*")
       @message[:metadata][:extension] = File.extname(@file_name).gsub('.','')
-      @message[:metadata][:processingNotes] = [Faker::Lorem.sentences(rand(2)+1).join(" "), nil ].sample
+      @message[:metadata][:processingNotes] = [Faker::Lorem.sentences(number: rand(2)+1).join(" "), nil ].sample
 
       # These change type in newer versions
-      @message[:metadata][:conditionNotes] = [Faker::Lorem.sentences(rand(2)+1).join(" "), nil ].sample
-      @message[:metadata][:otherNotes] = [Faker::Lorem.sentences(rand(2)+1).join(" "), nil ].sample
+      @message[:metadata][:conditionNotes] = [Faker::Lorem.sentences(number: rand(2)+1).join(" "), nil ].sample
+      @message[:metadata][:otherNotes] = [Faker::Lorem.sentences(number: rand(2)+1).join(" "), nil ].sample
 
     elsif @options[:message_version] == "2.0"
       # new keys
@@ -63,7 +63,7 @@ private
       @message[:metadata][:bnumber] = "B-#{(1...10000).to_a.sample}"
 
       # Values have changed since v 1.0
-      note_proc = Proc.new{ Faker::Lorem.sentences(rand(2)+1).join(" ") }
+      note_proc = Proc.new{ Faker::Lorem.sentences(number: rand(2)+1).join(" ") }
       @message[:metadata][:conditionNotes] = [[note_proc.call, note_proc.call], []].sample
       @message[:metadata][:otherNotes] = [[note_proc.call, note_proc.call], []].sample
 
@@ -96,7 +96,7 @@ private
       :pathToBag     => @bag_path,
       :checksum      => fake_checksum.to_s,
       :metadata      => {
-        :title => Faker::Lorem.words(rand(6)+1).join,
+        :title => Faker::Lorem.words(number: rand(6)+1).join,
         :identifier => "ABVVGTU".split("").shuffle.join,
         :typeOfResource => "moving image",
         :dateCreated => ["05/18/2015","07/20/1999","2/12/98E","06/07/77M",nil,""].sample,
@@ -109,11 +109,11 @@ private
         :fileFormat         => ['MPEG-4', 'MOV', 'OGG', nil].sample,
         :audioCodecName     => ['ALAC', 'API', 'WMAL', 'AAL', nil].sample,
         :videoCodecName     => ['AJA 10-bit 4:2:2 Component YCbCr', nil, "DXA", 'non-ISO MPEG-2.5'].sample,
-        :contentNotes     => [Faker::Lorem.sentences(rand(2)+1).join(" "), nil ].sample,
-        :accessNote       => [Faker::Lorem.sentences(rand(2)+1).join(" "), nil ].sample,
+        :contentNotes     => [Faker::Lorem.sentences(number: rand(2)+1).join(" "), nil ].sample,
+        :accessNote       => [Faker::Lorem.sentences(number: rand(2)+1).join(" "), nil ].sample,
         :duration         => ["00:16:25:23", "01:1#{rand{8}+1}:25:23", "01:16:25:#{rand{9}}#{rand{9}}"].sample,
         :dateCaptured     => Faker::Date.backward(1000).strftime("%-m/%d/%y"),
-        :projectIdentifier => "#{Faker::Lorem.words(1).first}.xls",
+        :projectIdentifier => "#{Faker::Lorem.words(number: 1).first}.xls",
         :formerClassmark   => "* #{('A'..'Z').to_a.shuffle[0..4].join} #{('1'..'9').to_a.shuffle[0..2].join}"
         }
      }
